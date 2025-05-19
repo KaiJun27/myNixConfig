ede067181c8878f5181b9e949ba147891cc3a529
KJ <a@a.com>
2025-05-19 20:59:30 +0800

    Added curl, ripgrep, and wget to system

--- nixos/default.nix
+++ nixos/default.nix
@@ -97,7 +97,10 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  curl
   git
+  ripgrep
+  wget
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
